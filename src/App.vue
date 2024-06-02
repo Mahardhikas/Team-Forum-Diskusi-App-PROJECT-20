6da8d50ceaf6ab2df0b697379a152119e2dbad05
Mahardhikas <202210081@student.ibik.ac.id>
2024-06-02 21:11:27 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HomePage/>
+  <Home/>
+  <users/>
+</template>
+
+<script>
+  import HomePage from "./components/HomePage.vue"
+  import Home from "./components/Home.vue"
+  import users from "./components/users.vue"
+export default {
+  name: 'App',
+  components: {
+      HomePage,
+      Home,
+      users
+     
+  }
+}
+</script>
+
+<style>
+ 
+</style>
